a843868a5ef005129c07e27a203ea3fd569fb1b1
ShraddhaB <sbhingarde2017@gmail.com>
2019-02-22 19:43:42 +0530

    Bonus

--- src/test/java/com/spp/scripts/Bonus.java
+++ src/test/java/com/spp/scripts/Bonus.java
@@ -109,7 +109,7 @@ public class Bonus extends BaseTest{
 		Assert.assertEquals(t, "Bonus successfully deleted");
 	}*/
 	
-	@Test(priority=7)
+	/*@Test(priority=7)
  	public void AddNewBonusDefinition_SC_638() throws Exception{
 		AddNewBonusDefinition sh=new AddNewBonusDefinition(driver);
 		sh.clickSalary();
@@ -4879,7 +4879,7 @@ public class Bonus extends BaseTest{
 		Assert.assertEquals(t, "Bonus successfully created");
 		cb.clickDeleteButton();
 		cb.switchToPopUpAndAccept(driver);
-	}
+	}*/
 	/*@Test(priority=217)
 	public void CreateBonusDefinitionConsideremployeeswithminimumworkingMaxBonusNearest10Rupee_SC_851() throws Exception {
 		CreateBonusDefinitionFormulaCalculateModeofPaymentChequeNearest10Rupee cb = new CreateBonusDefinitionFormulaCalculateModeofPaymentChequeNearest10Rupee(driver);
@@ -5551,7 +5551,7 @@ public class Bonus extends BaseTest{
 		cb.clickDeleteButton();
 		cb.switchToPopUpAndAccept(driver);		
 	}*/
-	/*@Test(priority=247)
+	@Test(priority=247)
 	public void CreateBonusDefinitionMaxBonusAllowManualEditingCashNearest10Rupee_SC_881() throws Exception {
 		CreateBonusDefinitionFormulaCalculateModeofPaymentChequeNearest10Rupee cb = new CreateBonusDefinitionFormulaCalculateModeofPaymentChequeNearest10Rupee(driver);
 		cb.clickSalary();
@@ -5787,7 +5787,7 @@ public class Bonus extends BaseTest{
 		cb.clickDeleteButton();
 		cb.switchToPopUpAndAccept(driver);	
 	}
-	@Test(priority=257)
+	/*@Test(priority=257)
 	public void AddNewBonusDefinitionMaxBonusModeofPaymentCashCurrentMonthNearest10Rupee_SC_891() throws Exception {
 		CreateBonusDefinitionFormulaCalculateModeofPaymentChequeNearest10Rupee cb = new CreateBonusDefinitionFormulaCalculateModeofPaymentChequeNearest10Rupee(driver);
 		cb.clickSalary();
