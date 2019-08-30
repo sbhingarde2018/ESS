42969b5d454e3a5ba212b449e3d5bfde993232ed
sbhingarde2018 <sbhingarde2017@gamil.com>
2019-08-30 12:01:41 +0530

    uodated

---
+++ SPPCloud.bat
@@ -0,0 +1,4 @@
+set JAVA_HOME=C:\Progra~1\Java\jdk1.8.0_181
+set CLASSPATH=.\robotan-ktwo_lib\javax.mail.jar;
+set ANT_HOME=.\robotan-ktwo_lib\apache-ant-1.10.3
+%ANT_HOME%\BIN\ANT.BAT -f SPPCloud.xml
