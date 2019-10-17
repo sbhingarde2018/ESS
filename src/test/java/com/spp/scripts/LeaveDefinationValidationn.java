0c07a00fe1a199a4e9460aeb0d40afbe7f984556
RSL <RSL@L430.Dlink>
2019-10-17 17:35:34 +0530

    Leave defination validation

---
+++ src/test/java/com/spp/scripts/LeaveDefinationValidationn.java
@@ -0,0 +1,54 @@
+package com.spp.scripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.spp.common.BaseTest;
+import com.spp.pages.LeaveDefinationValidation;
+
+public class LeaveDefinationValidationn extends BaseTest {
+	
+
+	@Test(priority=0)
+	public void AdvanceMasterValidation_sc_1760() throws Exception{
+		
+		LeaveDefinationValidation ld = new LeaveDefinationValidation(driver);
+		ld.navigateMaster();
+		
+		ld.clickOnleavedefinitions();
+		ld.enterLeaveName("");
+		ld.enterShortName("");
+		ld.clickOncreateLeave();
+		String s1 = ld.getLeaveNameValidationMessage();
+		Assert.assertEquals(s1, "Leave Name can't be blank");
+		String s2 = ld.getShortNameValidationMessage();
+		Assert.assertEquals(s2, "Short Name can't be blank");
+		
+		Thread.sleep(5000);
+		
+		ld.navigateMaster();
+		ld.clickOnleavedefinitions();
+		ld.enterLeaveName("CausalLeave_Bangalore");
+		ld.enterShortName("CL_BGR");
+		ld.clickOncreateLeave();
+		String s3 = ld.getSuccessMessage();
+		Assert.assertEquals(s3,"Leave definition successfully created." );
+		
+		Thread.sleep(5000);
+		
+		ld.navigateMaster();
+		ld.clickOnleavedefinitions();
+		ld.enterLeaveName("CausalLeave_Bangalore");
+		ld.enterShortName("CL_BGR");
+		ld.clickOncreateLeave();
+		String s4 = ld.getLeaveErrrorMessage();
+		Assert.assertEquals(s4, "Leave name has already been taken");
+		String s5 = ld.getShortNameErrrorMessage();
+		Assert.assertEquals(s5, "Short name has already been taken");
+		
+		Thread.sleep(5000);
+		
+	}
+
+	
+}
